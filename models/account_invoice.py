a0c1283f6c2dcadc16b8e512eede3f7317cadf23
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-10-30 11:13:42 -0600

    [IMP] add self_id

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -6,6 +6,10 @@ from odoo import api, fields, models, _
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
+    self_id = fields.Many2one(
+        comodel_name='account.invoice',
+        compute='_get_self_id',
+    )
     third_parties = fields.Float(
     )
     net_total_amount = fields.Float(
@@ -16,3 +20,7 @@ class AccountInvoice(models.Model):
     def _get_net_total_amount(self):
         for record in self:
             record.net_total_amount = record.amount_untaxed_invoice_signed - record.third_parties
+
+    def _get_self_id(self):
+        for record in self:
+            record.self_id = self.browse(record.id)
