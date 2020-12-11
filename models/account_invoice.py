baa89f2353145a16475802763249d53dca67deb3
Moisés Navarro <moisalejandro@gmail.com>
2020-12-11 14:52:38 -0600

    [REF]

--- models/account_invoice.py
+++ models/account_invoice.py
@@ -4,20 +4,19 @@ from odoo import api, fields, models, _
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = "account.invoice"
 
     self_id = fields.Many2one(
-        comodel_name='account.invoice',
-        compute='_get_self_id',
+        comodel_name="account.invoice",
+        compute="_get_self_id",
     )
-    third_parties = fields.Float(
-    )
+    third_parties = fields.Float()
     net_total_amount = fields.Float(
-        compute='_get_net_total_amount',
+        compute="_get_net_total_amount",
         store=True,
     )
 
-    @api.depends('third_parties', 'amount_untaxed')
+    @api.depends("third_parties", "amount_untaxed")
     def _get_net_total_amount(self):
         for record in self:
             record.net_total_amount = record.amount_untaxed - record.third_parties
